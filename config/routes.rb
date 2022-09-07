b9ccd3f8e1eade4aede5babcc1221d10f69f52b1
Gregory <gregory.laigneau@protonmail.com>
2022-09-07 15:41:17 +0200

    Fix missing curly bracket and devise_for clone

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
     omniauth_callbacks: 'users/omniauth_callbacks'
+  }
   resources :projects
-  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root 'home#index'
 end
