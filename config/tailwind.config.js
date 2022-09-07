88179ffac4370aa638f8b90999bf37e23324020d
pandaka87 <noellie.peuch2@gmail.com>
2022-09-07 15:45:56 +0200

    Initialize Tailwind theme

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -14,6 +14,23 @@ module.exports = {
         Cantarell: ["Cantarell", ...defaultTheme.fontFamily.sans],
         sans: ["Inter var", ...defaultTheme.fontFamily.sans],
       },
+      textColor: {
+        skin: {
+          base: 'var(--color-text-base)',
+          muted: 'var(--color-text-muted)',
+          inverted: 'var(--color-text-inverted)',
+        }
+      },
+      backgroundColor: {
+        skin: {
+          'button-accent': 'var(--color-button-accent)',
+          'button-accent-hover': 'var(--color-button-accent-hover)',
+          'button-muted': 'var(--color-button-muted)',
+          'button-muted-hover': 'var(--color-button-muted-hover)',
+          'button-warning': 'var(--color-button-warning)',
+          'button-warning-hover': 'var(--color-button-warning-hover)'
+        }
+      }
     },
   },
   plugins: [
