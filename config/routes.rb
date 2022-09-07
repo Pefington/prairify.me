918d817551651b784a46579accf332eeddbc9808
Gregory <gregory.laigneau@protonmail.com>
2022-09-07 17:15:27 +0200

    Add static pages to routes and modify root

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,11 @@ Rails.application.routes.draw do
     omniauth_callbacks: 'users/omniauth_callbacks'
   }
   resources :projects
+
+  scope '/', controller: :static_pages do
+    get :team, :about, :contact, :blog
+  end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  root 'home#index'
+  root 'static_pages#home'
 end
