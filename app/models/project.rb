8c473148070b8d83fe1311c770ec7eaf864eb6c2
Sam <104089857+Samsou1@users.noreply.github.com>
2022-09-08 17:51:32 +0200

    Search front (#17)

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,7 @@
 class Project < ApplicationRecord
-  has_many :join_table_project_plants
+  has_many :join_table_project_plantss
+  has_many :users, through: :likes
   has_many_attached :photos
   belongs_to :user
+
 end
