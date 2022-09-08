7a05788082e9860e92cae45096bfc92eecfd913d
Samsou1 <sam.moreau0@gmail.com>
2022-09-08 16:56:00 +0200

    search front

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,7 @@ Rails.application.routes.draw do
     omniauth_callbacks: 'users/omniauth_callbacks'
   }
   resources :projects
-  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  root 'home#index'
   resources :search, only: %i[index]
   resources :like, only: %i[new create destroy]
   scope '/', controller: :static_pages do
