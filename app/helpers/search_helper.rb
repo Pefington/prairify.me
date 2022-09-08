471fcddd87f675d612cbe3d1a9a1f2ad5b198f62
Pierre-François Salmand <maabaa@gmail.com>
2022-09-09 03:11:13 +0700

    Refactor SearchHelper

--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -1,51 +1,40 @@
 module SearchHelper
-
   def get_place(place)
     place_url = "https://api.inaturalist.org/v1/search?q=#{place}&sources=places&per_page=1"
-    place_data = JSON.load(URI.open(place_url))['results'][0]
-    if place_data != nil
-      if place_data['matches'] != nil
-        [place_data['matches'].join(" "), place_data['record']['id']]
-      else
-        nil
-      end
-    else
-      nil
+    place_data = JSON.parse(URI.parse(place_url).read)['results'][0]
+    return if place_data.nil?
+
+    if place_data['matches']
+      [place_data['matches'].join(' '), place_data['matches']['id']]
+    elsif place_data['record']
+      [place_data['record']['display_name'], place_data['record']['id']]
     end
   end
 
   def get_place_name(place)
-    if get_place(place) != nil
-      get_place(place)[0]
-    else
-      nil
-    end
+    get_place(place)[0] unless get_place(place).nil?
   end
 
   def get_place_id(place)
-    if get_place(place) != nil
-      get_place(place)[1]
-    else
-      nil
-    end
+    get_place(place)[1] unless get_place(place).nil?
   end
 
   def get_data(place, number_of_elements)
     place_id = get_place_id(place)
-    return nil if place_id == nil
+    return nil if place_id.nil?
+
     taxa_url = "https://api.inaturalist.org/v1/taxa?is_active=true&taxon_id=47126&rank_level=20&per_page=#{number_of_elements}&preferred_place_id=#{place_id}&all_names=true"
-    taxa_data = JSON.load(URI.open(taxa_url))['results']
+    taxa_data = JSON.parse(URI.parse(taxa_url).read)['results']
     results = []
     taxa_data.size.times do |index|
       hash_data = {}
-      hash_data[:id] = taxa_data[index]["id"]
-      hash_data[:common_name] = taxa_data[index]["preferred_common_name"].capitalize
-      hash_data[:scientific_name] = taxa_data[index]["name"]
-      hash_data[:wiki] = taxa_data[index]["wikipedia_url"]
-      hash_data[:picture_url] = taxa_data[index]["default_photo"]["medium_url"]
+      hash_data[:id] = taxa_data[index]['id']
+      hash_data[:common_name] = taxa_data[index]['preferred_common_name'].capitalize
+      hash_data[:scientific_name] = taxa_data[index]['name']
+      hash_data[:wiki] = taxa_data[index]['wikipedia_url']
+      hash_data[:picture_url] = taxa_data[index]['default_photo']['medium_url']
       results.push(hash_data)
     end
     results
   end
-
 end
