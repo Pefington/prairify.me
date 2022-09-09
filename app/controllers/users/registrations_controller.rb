1c0f9d4e47b8614b486039ee5443178506f64408
Gregory <gregory.laigneau@protonmail.com>
2022-09-10 00:04:03 +0200

    Modify profile without current password for google_oauth

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -38,7 +38,15 @@ class Users::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
-  # protected
+  protected
+
+  def update_resource(resource, params)
+    if current_user.provider == 'google_oauth2'
+      resource.update_without_password(params.except('current_password'))
+    else
+      resource.update_with_password(params)
+    end
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_up_params
