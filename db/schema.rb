3e15ee059905444e20b2c7ccb84ba6d288c1903c
Sam <104089857+Samsou1@users.noreply.github.com>
2022-09-12 15:37:33 +0200

    Back/projectupdate (#53)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_12_080150) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_12_120029) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -40,6 +41,16 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_12_080150) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string "content"
+    t.bigint "project_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["project_id"], name: "index_comments_on_project_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "favourites", force: :cascade do |t|
@@ -106,6 +117,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_12_080150) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "comments", "projects"
+  add_foreign_key "comments", "users"
   add_foreign_key "favourites", "projects"
   add_foreign_key "favourites", "users"
   add_foreign_key "likes", "projects"
