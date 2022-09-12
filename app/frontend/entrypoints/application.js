9ac1a92d34469bf2272357cacf0cc76bc07b3b7e
Pierre-François Salmand <maabaa@gmail.com>
2022-09-12 16:04:44 +0700

    Move Alpine to package install

--- app/frontend/entrypoints/application.js
+++ app/frontend/entrypoints/application.js
@@ -5,17 +5,17 @@
 //    <%= vite_javascript_tag 'application' %>
 console.log("Vite ⚡️ Rails");
 
+import Alpine from 'alpinejs'
+
+window.Alpine = Alpine
+
+Alpine.start()
+
 // If using a TypeScript entrypoint file:
 //     <%= vite_typescript_tag 'application' %>
 //
 // If you want to use .jsx or .tsx, add the extension:
 //     <%= vite_javascript_tag 'application.jsx' %>
-
-console.log(
-  "Visit the guide for more information: ",
-  "https://vite-ruby.netlify.app/guide/rails"
-);
-
 // Example: Load Rails libraries in Vite.
 //
 // import * as Turbo from '@hotwired/turbo'
