05167ada1ecd751924367a0bff7cfe40aedf975b
PierreAND <andrieu@gmail.com>
2022-09-12 21:08:54 +0200

    model user done

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,11 +11,16 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :projects, through: :likes
   has_many :favourites, dependent: :destroy
+    
+  validates :username, :password,
+  presence: true,
+	length: { in: 6..15 },
+	format: { without: /[!@#%*+;,?&()=]/ }
 
-  after_create :welcome_send
-  def welcome_send
-    UserMailer.welcome_email(self).deliver_now
-  end
+  # after_create :welcome_send
+  # def welcome_send
+  #   UserMailer.welcome_email(self).deliver_now
+  # end
 
   enum role: { user: 0, admin: 1 }
 
