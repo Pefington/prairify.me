a85f0a4dc7784f1864b3b289fba7a43d8d070fe9
Gregory <gregory.laigneau@protonmail.com>
2022-09-13 17:23:22 +0200

    Create users controller

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,23 +1,52 @@
 class Admin::UsersController < Admin::AdminController
   def index
-    @users = User.all
+    @users = User.order(id: :asc).page(params[:page])
   end
 
   def edit
-  end
-
-  def show
+    @user = User.find(params[:id])
   end
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(new_secure_params)
+    if @user.save
+      flash[:success] = 'The account has been created'
+      redirect_to admin_users_path
+    else
+      flash[:error] = "The account couldn't be created"
+      render :new
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(edit_secure_params)
+      flash[:success] = 'The account has been successfully updated.'
+      redirect_to admin_users_path
+    else
+      flash[:error] = "The account couldn't be updated"
+      render :edit
+    end
   end
 
   def destroy
     @user = User.find(params[:id])
     @user.destroy
-    redirect_to admin_users_path, notice: 'Project was successfully destroyed.'
+    flash[:success] = 'The account has been successfully destroyed.'
+    redirect_to admin_users_path
+  end
+
+  private
+
+  def edit_secure_params
+    params.require(:user).permit(:email, :username, :role)
+  end
+
+  def new_secure_params
+    params.require(:user).permit(:email, :username, :role, :password, :password_confirmation)
   end
 end
