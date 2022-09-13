5071dd6a5c083020a57893af60c99f689751beff
Pierre-François Salmand <maabaa@gmail.com>
2022-09-13 21:22:53 +0700

    WIP

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,8 @@
 class SearchController < ApplicationController
   def index
+    selected_plants = SelectedPlant.where(user_id: current_user&.id)
+    @selected_plants = selected_plants.map(&:inaturalist_id)
+
     @place = helpers.get_place_name(params[:search])
     @local_plants = helpers.get_data(params[:search])
     @hits = @local_plants.count
--- app/controllers/selected_plants_controller.rb
+++ app/controllers/selected_plants_controller.rb
@@ -1,5 +1,6 @@
 class SelectedPlantsController < ApplicationController
   before_action :set_selected_plant, only: %i[destroy]
+  skip_before_action :verify_authenticity_token, only: [:reset]
 
   def create
     @selected_plant = SelectedPlant.new(selected_plant_params)
@@ -7,15 +8,20 @@ class SelectedPlantsController < ApplicationController
     Rails.logger.debug params.inspect
 
     if @selected_plant.save
-      flash[:success] = 'Selected plant was successfully created.'
+      flash.now[:success] = 'Plant was successfully selected.'
     else
-      flash[:alert] = 'Plant is already selected.'
+      flash.now[:alert] = 'Plant was not selected due to an error.'
     end
   end
 
   def destroy
     @selected_plant.destroy
-    flash[:success] = 'Selected plant was successfully created.'
+    flash.now[:success] = 'Plant was successfully removed.'
+  end
+
+  def reset
+    SelectedPlant.where(user_id: current_user.id).destroy_all
+    flash.now[:notice] = 'Selected plants were successfully reset.'
   end
 
   private
