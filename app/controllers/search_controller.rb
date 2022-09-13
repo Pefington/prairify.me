b473eba11ce4e5ee5e6d858b78a980698e5b92a6
Pierre-François Salmand <71193709+Pefington@users.noreply.github.com>
2022-09-13 16:50:42 +0700

    WIP - Implement plant selection for a new project (#38)

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,12 @@
 class SearchController < ApplicationController
   def index
     @place = helpers.get_place_name(params[:search])
-    @plants = helpers.get_data(params[:search], 120)
+    @local_plants = helpers.get_data(params[:search])
+    @selected_plants = []
+  end
+
+  def add_plant
+    @selected_plants.push(params[:plant][:id])
+    render :index
   end
 end
---
+++ db/migrate/20220912161148_create_selected_plants.rb
@@ -0,0 +1,10 @@
+class CreateSelectedPlants < ActiveRecord::Migration[7.0]
+  def change
+    create_table :selected_plants do |t|
+      t.references :user, null: false, foreign_key: true
+      t.integer :inaturalist_id
+
+      t.timestamps
+    end
+  end
+end
