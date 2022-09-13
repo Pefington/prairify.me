5e1fb9c9b9ddb34b0201cbee3821921a452da26b
Samsou1 <sam.moreau0@gmail.com>
2022-09-13 21:15:56 +0200

    delete useless controler methods + ass. views

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,15 @@
 Rails.application.routes.draw do
-  resources :selected_plants
-  resources :project_updates
   root 'static_pages#home'
   devise_for :users, controllers: {
     omniauth_callbacks: 'users/omniauth_callbacks',
     registrations: 'users/registrations'
   }
   resources :projects do
-    resources :project_updates, only: %i[index new create destroy]
-    resources :comments, only: %i[index new create destroy]
+    resources :project_updates, only: %i[new create destroy]
+    resources :comments, only: %i[new create destroy]
   end
-
+  
+  resources :selected_plants
   resources :likes, only: %i[create destroy]
   resources :favourites, only: %i[index create destroy]
   resources :search, only: %i[index]
