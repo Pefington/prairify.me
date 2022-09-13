253bb6ce4a4a474aaf2adf63c44d0e08d9aaa8e5
Gregory <gregory.laigneau@protonmail.com>
2022-09-13 17:22:27 +0200

    Create dashboard controller

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,24 +1,19 @@
 class Admin::DashboardController < Admin::AdminController
   def index
     @users = User.all
-    @latest_users = User.order('id ASC').reverse_order.last(10)
+
+    @latest_users = User.order('id desc').first(10)
+
     @projects = Project.all
-    @latest_projects = Project.order('id ASC').reverse_order.last(10)
-    @last_users_seven_days = User.where(
-      'created_at >= :seven_days_ago',
-      seven_days_ago: 7.days.ago
-    ).count
-    @last_users_thirty_days = User.where(
-      'created_at >= :thirty_days_ago',
-      thirty_days_ago: 30.days.ago
-    ).count
-    @last_projects_seven_days = Project.where(
-      'created_at >= :seven_days_ago',
-      seven_days_ago: 7.days.ago
-    ).count
-    @last_projects_thirty_days = Project.where(
-      'created_at >= :thirty_days_ago',
-      thirty_days_ago: 30.days.ago
-    ).count
+
+    @latest_projects = Project.order('id desc').first(10)
+
+    @users_week = User.where('created_at > ?', 1.week.ago).count
+
+    @users_month = User.where('created_at > ?', 1.month.ago).count
+
+    @projects_week = Project.where('created_at > ?', 1.week.ago).count
+
+    @projects_month = Project.where('created_at > ?', 1.month.ago).count
   end
 end
