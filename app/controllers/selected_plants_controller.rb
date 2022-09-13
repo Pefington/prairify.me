3a45b6e2ac2541ee9e9671071c3fd63557c80008
Sam <104089857+Samsou1@users.noreply.github.com>
2022-09-13 18:06:07 +0200

    Back/search (#61)

--- app/controllers/selected_plants_controller.rb
+++ app/controllers/selected_plants_controller.rb
@@ -1,23 +1,18 @@
 class SelectedPlantsController < ApplicationController
   before_action :set_selected_plant, only: %i[show edit update destroy]
 
-  # GET /selected_plants
   def index
     @selected_plants = SelectedPlant.all
   end
 
-  # GET /selected_plants/1
   def show; end
 
-  # GET /selected_plants/new
   def new
     @selected_plant = SelectedPlant.new
   end
 
-  # GET /selected_plants/1/edit
   def edit; end
 
-  # POST /selected_plants
   def create
     @selected_plant = SelectedPlant.new(selected_plant_params)
     @selected_plant.user_id = current_user.id
@@ -30,7 +25,6 @@ class SelectedPlantsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /selected_plants/1
   def update
     if @selected_plant.update(selected_plant_params)
       redirect_to @selected_plant, notice: 'Selected plant was successfully updated.'
@@ -39,7 +33,6 @@ class SelectedPlantsController < ApplicationController
     end
   end
 
-  # DELETE /selected_plants/1
   def destroy
     @selected_plant.destroy
     redirect_to selected_plants_url, notice: 'Selected plant was successfully destroyed.'
@@ -47,12 +40,10 @@ class SelectedPlantsController < ApplicationController
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_selected_plant
     @selected_plant = SelectedPlant.find(params[:id])
   end
 
-  # Only allow a list of trusted parameters through.
   def selected_plant_params
     Rails.logger.debug params.inspect
     params.permit(:user_id, :inaturalist_id)
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,8 @@
 class Comment < ApplicationRecord
   belongs_to :project
   belongs_to :user
-
+  validates :user, presence: true
+  validates :project, presence: true
   validates :content, presence: true,
-                      length: { in: 10..150 }
+                      length: { in: 5..150 }
 end
