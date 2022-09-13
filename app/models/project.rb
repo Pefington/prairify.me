0899e640aa2f6fb7e34f410f0373fe5c8875a10e
Pierre-François Salmand <71193709+Pefington@users.noreply.github.com>
2022-09-13 23:54:38 +0700

    Remove restrictive validations (#63)

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,16 +3,14 @@ class Project < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :favourites, dependent: :destroy
   has_many :comments, dependent: :destroy
-  has_many :likes, dependent: :destroy
   has_many :project_updates, dependent: :destroy
   has_many_attached :photos, dependent: :destroy
   belongs_to :user
-  
+
   after_create :project_post
 
   def project_post
     UserMailer.email_project(self).deliver_now
     AdminMailer.project_email(self).deliver_now
   end
-
 end
