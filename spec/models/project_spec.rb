a43f33f199ff024fc4b5aa9686615843440219fa
PierreAND <andrieu@gmail.com>
2022-09-13 09:49:44 +0200

    new association model project

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,10 +1,9 @@
-
 require 'rails_helper'
 
 RSpec.describe Project, type: :model do
- before do
-  @project = FactoryBot.create(:project)
-end 
+  before do
+    @project = FactoryBot.create(:project)
+  end
 
   context 'validation' do
     it 'has a valid factory' do
@@ -13,6 +12,15 @@ end
 
     it 'is valid with valid attributes' do
       expect(@project).to be_a(Project)
+    end
+
+    context 'associations' do
+      describe '#projects' do
+        it { expect(@project).to have_many(:comments) }
+        it { expect(@project).to have_many(:likes) }
+        it { expect(@project).to have_many(:favourites) }
+        it { expect(@project).to have_many(:plants) }
+      end
     end
   end
 end
