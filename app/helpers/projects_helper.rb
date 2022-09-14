16337ef3e12c479fe0679161f5dfcfb1b105a926
Samsou1 <sam.moreau0@gmail.com>
2022-09-14 11:41:35 +0200

    get rid of find_by s

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -31,15 +31,15 @@ module ProjectsHelper
     when 'recent'
       return Project.all.reverse
     when 'most_liked'
-      hash = sort_likes.sort_by {|k, v| -v}
+      hash = group_likes_by_project.sort_by {|k, v| -v}
       create_right_order_of_display(hash)
     when 'least_liked'
-      hash = sort_likes.sort_by {|k, v| v}
+      hash = group_likes_by_project.sort_by {|k, v| v}
       create_right_order_of_display(hash)
     end
   end
 
-  def sort_likes
+  def group_likes_by_project
     likes = Like.all
     hash = {}
     Project.all.length.times do |index|
@@ -54,7 +54,7 @@ module ProjectsHelper
   def create_right_order_of_display(hash)
     result = []
     hash.each_with_index do |like, index|
-      result.push(Project.find_by(id:hash[index][0]))
+      result.push(Project.find(hash[index][0]))
     end
     result
   end
