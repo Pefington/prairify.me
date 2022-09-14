1c944cea4c69960a45f47cc9b28abd6a39c2e476
Pierre-François Salmand <71193709+Pefington@users.noreply.github.com>
2022-09-15 02:08:20 +0700

    Update project_update.rb

--- app/models/project_update.rb
+++ app/models/project_update.rb
@@ -1,5 +1,6 @@
 class ProjectUpdate < ApplicationRecord
   belongs_to :project
+  validates :project, presence: true
   validates :title, presence: true
   validates :description, presence: true
   has_one_attached :photo
