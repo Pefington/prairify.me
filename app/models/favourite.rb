18e23adc7fb8adc6b50bf65d67d02b2fd05b49e4
Pierre-François Salmand <71193709+Pefington@users.noreply.github.com>
2022-09-15 02:06:20 +0700

    Update favourite.rb

--- app/models/favourite.rb
+++ app/models/favourite.rb
@@ -1,5 +1,7 @@
 class Favourite < ApplicationRecord
   belongs_to :user
   belongs_to :project
+  validates :project, presence: true
+  validates :user, presence: true
   validates :user, uniqueness: { scope: :project }
 end
