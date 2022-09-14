2c17ef97a5f6d6cb9187e7c72c5261f22bd0d5a9
Noellie Peuch <108153483+pandaka87@users.noreply.github.com>
2022-09-14 23:09:12 +0200

    Front/homepage/caroussel (#76)
    Co-authored-by: Pierre-François Salmand <maabaa@gmail.com>

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,8 @@
 class StaticPagesController < ApplicationController
+  def home
+    @projects = helpers.latest_projects
+  end
+
   def profile
     @profile = current_user
     @projects = helpers.current_user_projects
