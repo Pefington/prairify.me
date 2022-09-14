909676d41028aa73ddd8645988f217537eebcf15
Pierre-François Salmand <71193709+Pefington@users.noreply.github.com>
2022-09-15 02:07:27 +0700

    Update like.rb

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,5 +1,7 @@
 class Like < ApplicationRecord
   belongs_to :user
   belongs_to :project
+  validates :project, presence: true
+  validates :user, presence: true
   validates :user, uniqueness: { scope: :project }
 end
