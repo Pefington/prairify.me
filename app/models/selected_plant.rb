7941f1564606431ea011b2ae8b8019002b38d97f
Pierre-François Salmand <71193709+Pefington@users.noreply.github.com>
2022-09-15 02:09:43 +0700

    Update selected_plant.rb

--- app/models/selected_plant.rb
+++ app/models/selected_plant.rb
@@ -1,5 +1,6 @@
 class SelectedPlant < ApplicationRecord
   belongs_to :user
+  validates :user, presence: true
   validates :inaturalist_id, presence: true
   validates :inaturalist_id, uniqueness: { scope: :user }
 end
