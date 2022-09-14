7fa9b18aad654a8cc0c849a4b8078ebf6a857e6a
Pierre-François Salmand <maabaa@gmail.com>
2022-09-15 04:36:05 +0700

    Achieve working i18n implementation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,7 +15,16 @@ class ApplicationController < ActionController::Base
 
   private
 
+  def default_url_options
+    { locale: I18n.locale }
+  end
+
   def set_locale
-    I18n.locale = params[:locale] if params[:locale]
+    I18n.locale = extract_locale || I18n.default_locale
+  end
+
+  def extract_locale
+    parsed_locale = params[:locale]
+    I18n.available_locales.map(&:to_s).include?(parsed_locale) ? parsed_locale.to_sym : nil
   end
 end
