41d19d721f2e0e36b5df7f0a5e4420cbc5c078b7
PierreAND <andrieu@gmail.com>
2022-09-14 16:41:15 +0200

    config controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,17 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  before_action :set_locale
+
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:email, :username, :password, :current_password) }
+  end
+
+  private
+
+  def set_locale
+    I18n.locale = params[:locale] if params[:locale]
   end
 end
