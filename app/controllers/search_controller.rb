01f0898201dc031fe1984ca179d0629f930eb146
Samsou1 <sam.moreau0@gmail.com>
2022-09-14 16:51:23 +0200

    tests puts geoloc

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,9 @@
 class SearchController < ApplicationController
   def index
     if params[:search] == 'geoloc'
+      puts request.location.region
+      puts helpers.usable_url(request.location.region)
+      puts helpers.get_place_name(helpers.usable_url(request.location.region))
       @place = helpers.get_place_name(helpers.usable_url(request.location.region))
       @local_plants = helpers.get_data(helpers.usable_url(request.location.region))
     else
