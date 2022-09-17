a2dd5400133367822599a20a155de4c7dd3c6c98
Pierre-François Salmand <maabaa@gmail.com>
2022-09-17 23:38:45 +0700

    Activate Mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,8 +51,9 @@ Rails.application.configure do
   # config.cache_store = :mem_cache_store
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.perform_deliveries = false
-  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { host: 'https://prairify.me' }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
